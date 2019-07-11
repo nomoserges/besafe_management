513147980c7915c44e8947a7b42d6ff4d2af9b45
nomoserges <nomoserges@gmail.com>
2019-07-11 15:50:00 +0200

    deleting

--- sources/models/notifications.js
+++
@@ -1,9 +0,0 @@
-export function getNotifications(){
-	return notifications;
-}
-
-const notifications = [
-	{ title:"Latest tasks in your projects", message:"Top margin in a popup window is smaller than the..." },
-	{ title:"Autocat report", message:"Navigations problem in the left menu" },
-	{ title:"Assignment", message:"Gita Noda assigned you a new ticket" }
-];
